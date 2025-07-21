52713f7286e8906451b64d8f1688634eac0027fb
Aldo Becerril <aldobecerril@google.com>
2025-07-21 07:54:34 -0700

    No public description
    PiperOrigin-RevId: 785435466

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.3
+
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import PackageDescription
+
+let package = Package(
+  name: "AppLovinAdapter",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(
+      name: "Adapter",
+      targets: ["Adapter"]
+    )
+  ],
+  targets: [
+    .target(
+      name: "AppLovinAdapterTarget",
+      dependencies: [
+        .target(name: "Adapter")
+      ],
+      path: "AppLovinAdapterTarget"
+    ),
+    .binaryTarget(
+      name: "Adapter",
+      url:
+        "https://dl.google.com/googleadmobadssdk/mediation/ios/applovin/AppLovinAdapter-13.3.1.0.zip",
+      checksum: "df18b69ad0d893cc64c5ca145c26d551621102311b849a30471b8e6855cdc6f0"
+    ),
+  ]
+)
